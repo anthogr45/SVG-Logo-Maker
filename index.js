68072f6bfee16940966b4f3b6a78b5fc862ee7c8
AnthoG <anthony.dswg@gmail.com>
2023-10-19 03:28:12 -0400

    With the classes

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
+const Circle = require('./lib/shapes/circle');
+const Triangle = require('./lib/shapes/triangle');
+const Square = require('./lib/shapes/square');
 // const SVG = require('svg.js');
 
 
 
-let logoChar = '';
-let logoShape = '';
-let colorname = '';
-let hexCode = '';
-let shapecolorName = '';
-let shapehexCode = '';
+let logoChar;
+let logoShape;
+let textColor;
+let hexCode;
+let shapeColor;
+let shapehexCode;
 
 
 
   
 function initOne () {
   // return new Promise ((resolve, reject) => {   
-   inquirer
-   .prompt ([ 
+  const prompQuestions = [
     {
      type: 'input',
      name: 'logo_letters',
@@ -24,173 +27,65 @@ function initOne () {
     },
     {
       type: 'list',
-      name: 'shapeType',
+      name: 'shapetype',
       message: 'Select a shape for the Logo:',
-      choices: ['circle', 'triangle', 'square'],
+      choices: ['Circle', 'Triangle', 'Square'],
     },
     
     {
-    type: 'list',
-    name: 'colorType',
-    message: 'Choose the type of color:',
-    choices: ['Color Name', 'Hexadecimal Code'],
-     },
-     
-  ])
-// const questions1 = ( ([
+    type: 'input',
+    name: 'textcolor',
+    message: 'Insert a color for the Logo letters [Color Name, Hexadecimal Code]:',
+ 
+    },
+
+    {
+      type: 'input',
+      name: 'shapecolor',
+      message: 'Insert a color for the Logo shape [Color Name, Hexadecimal Code]:',
    
-  .then((answers) => {
-    const colorType = answers.colorType;
+    },
+     
+  ]
+  inquirer.prompt(prompQuestions).then((answers) => {
+
     logoChar = answers.logo_letters;
-    logoShape = answers.shapeType;
+    textColor = answers.textcolor;
+    logoShape = answers.shapetype;
+    shapeColor = answers.shapecolor;
 
-    if (colorType === 'Color Name') {
-      // Prompt for color name
-      inquirer
-        .prompt([
-          {
-            type: 'list',
-            name: 'colorName',
-            message: 'Choose a color for the 3 letters:',
-            choices: ['Red', 'Green', 'Blue', 'Yellow', 'Pink', 'Black', 'White', 'Brown' ]
-          },
-          {
-            type: 'list',
-            name: 'shapecolor',
-            message: 'Choose a color for the Logo shape:',
-            choices: ['Red', 'Green', 'Blue', 'Yellow', 'Pink', 'Black', 'White', 'Brown' ]
-          },
+    let shapeObj;
+    switch (logoShape) {
+       case 'Circle':
+         shapeObj = new Circle(360, shapeColor);
+         break;
+       case 'Triangle' :
+         shapeObj = new Triangle(100, shapeColor);
+         break;
+       case 'Square' :
+         shapeObj = new Square(100, 100, shapeColor);
+         break;
+    }
 
-        ])
-        .then((answers) => {
-          colorname = answers.colorName;
-          shapecolorName = answers.shapecolor;
-        });
-    } else if (colorType === 'Hexadecimal Code'){
-       // Prompt for hexadecimal code
-       inquirer
-       .prompt([
-         {
-           type: 'input',
-           name: 'letterHexCode',
-           message: 'Enter a color hexadecimal code for the Logo letters:',
-         },
-         {
-           type: 'input',
-           name: 'shapehex',
-           message: 'Enter a hexadecimal code for the shape:',
-         },
-       ])
-       .then((answers) => {
-         hexCode = answers.letterHexCode;  
-         shapehexCode  = answers.shapehex;                   
-       });
-      }
-      
-  })
-  
+    const svgContent = `
+    <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100">
+    ${shapeObj.draw()}
+    <text x="50%" y="50%" text-anchor="middle" dy="0.3em" font-size="15" fill="${textColor}">ABC</text>
+   </svg>
+   `;
+   
+   // Save the svgContent to a file
+   fs.writeFile('logo.svg', svgContent, (err) => {
+     if (err) throw err;
+     console.log('Logo created as logo.svg');
+   });
+
+  });
+
 }
-
-
+   
 
 
   initOne();
-  // initTwo();
-// }
-
-// main();
-
-
-
-
-
-  // initShape ()
-  // callback ();
-
-
-
-
-
-// function initShape () {
-//   inquirer
-//     .prompt([
-//     {
-//       type: 'list',
-//       name: 'cshape',
-//       message: 'Select a shape for the Logo:',
-//       choices: ['circle', 'triangle', 'square'],
-//     },
-//     {
-//       type: 'list',
-//       name: 'shapeColor',
-//       message: 'Choose the type of color for the Logo shape:',
-//       choices: ['Color Name', 'Hexadecimal Code'],
-//     },
-//   ])
-//   .then((answers) => {
-//     shapecolorName = answers.shapeColor;
-
-//     if (colorType === 'Color Name') {
-//       // Prompt for color name
-//       inquirer
-//         .prompt([
-//           {
-//             type: 'list',
-//             name: 'shapecolorName',
-//             choices: ['Red', 'Green', 'Blue', 'Yellow', 'Pink', 'Black', 'White', 'Brown' ]
-//           },
-//         ])
-//         .then((answers) => {
-//           colorname = answers.colorname;
-//         });
-//     } else if (colorType === 'Hexadecimal Code'){
-//        // Prompt for hexadecimal code
-//        inquirer
-//        .prompt([
-//          {
-//            type: 'input',
-//            name: 'shapehexCode',
-//            message: 'Enter a hexadecimal code for the shape:',
-//          },
-//        ])
-//        .then((answers) => {
-//          shapehexCode = answers.shapehexCode;                 
-//        });
-//       }
-      
-//   });
-
-// }
-
-// init ();
-
-
-
-  
-  
  
 
-
-
-
-
-
-
-  // .then(answers => {
-  //   // Save the user's responses to a file or generate the SVG logo based on the input
-  // })
-  // .catch(error => {
-  //   console.log('Error:', error);
-  // });
-
-
-
-
-  // Create an SVG canvas
-// const canvas = SVG(document.documentElement);
-
-// Create a circle shape
-// const circle = canvas.circle(100).fill('blue').center(200, 200);
-
-// // Save the SVG to a file
-// canvas.svg({ filename: 'logo.svg' });
